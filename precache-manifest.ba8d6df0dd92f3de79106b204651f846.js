05b9823298ae45a456583c8a91489964133f9ec8
DavidScicluna\DavidScicluna <david.scicluna.a106106@mcast.edu.mt>
2020-01-10 17:51:25 +0100

    Updates

---
+++ precache-manifest.ba8d6df0dd92f3de79106b204651f846.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01d0a9d667fbdea87d7624340f950117",
+    "url": "/react-clock/index.html"
+  },
+  {
+    "revision": "3657f269f3afdbdac6e9",
+    "url": "/react-clock/static/css/main.b7482fd5.chunk.css"
+  },
+  {
+    "revision": "a29d74fea75438653e5c",
+    "url": "/react-clock/static/js/2.6d4ca715.chunk.js"
+  },
+  {
+    "revision": "9652715b819d26dcc09aad3261a56a36",
+    "url": "/react-clock/static/js/2.6d4ca715.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3657f269f3afdbdac6e9",
+    "url": "/react-clock/static/js/main.745c8016.chunk.js"
+  },
+  {
+    "revision": "ae6976bc6b5c9d0f3eb5",
+    "url": "/react-clock/static/js/runtime-main.d2a37bb1.js"
+  },
+  {
+    "revision": "5a020f674a2b7c661752664249c3934b",
+    "url": "/react-clock/static/media/beep.5a020f67.wav"
+  }
+]);
